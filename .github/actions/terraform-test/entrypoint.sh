e290d84c92e9525320cb6e7e1f02116881bb5ca4
cosmin_tudor <cosmin.tudor@oracle.com>
2019-11-04 14:33:57 +0200

    Fix terratest

--- .github/actions/terraform-test/entrypoint.sh
+++ .github/actions/terraform-test/entrypoint.sh
@@ -6,7 +6,7 @@ apt install -y build-essential unzip go-dep
 #Installing terraform and upgrading code to Terraform 12
 wget -q https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_amd64.zip
 unzip terraform_0.12.10_linux_amd64.zip -d /usr/bin
-cd ${GITHUB_WORKSPACE}/terraform-oci-compute-instance
+cd ${GITHUB_WORKSPACE}/examples/instance_default
 echo ${GITHUB_WORKSPACE}
 terraform init && terraform 0.12upgrade -yes
 
@@ -34,6 +34,6 @@ dep ensure
 #Set up environment to run the terraform code
 #echo "${TF_VAR_private_key}" > ${GITHUB_WORKSPACE}/oci.pem
 #export TF_VAR_private_key_path=${GITHUB_WORKSPACE}/oci.pem
-export TF_ACTION_WORKING_DIR=${GITHUB_WORKSPACE}/terraform-oci-compute-instance/examples/instance_default
+export TF_ACTION_WORKING_DIR=${GITHUB_WORKSPACE}/examples/instance_default
 echo ${TF_ACTION_WORKING_DIR}
 go test -v $HOME/go/src/terratest/test/tf-oci-instance_test.go -timeout 20m

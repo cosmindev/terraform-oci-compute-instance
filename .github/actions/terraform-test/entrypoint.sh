3648ed2955fd089ba5f431a882777c9f1a63a606
cosmin_tudor <cosmin.tudor@oracle.com>
2019-11-04 18:18:28 +0200

    test

--- .github/actions/terraform-test/entrypoint.sh
+++ .github/actions/terraform-test/entrypoint.sh
@@ -38,11 +38,13 @@ export TF_ACTION_WORKING_DIR=${GITHUB_WORKSPACE}/examples/instance_default
 TERRAFORM_VERSION="$(terraform --version)"
 echo "------->>>Terraform version = ${TERRAFORM_VERSION}"
 echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_tenancy_id}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_user_id}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_fingerprint}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_private_key}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_ssh_authorized_keys}"
+echo "------->>> TF_VAR_user_id= ${TF_VAR_user_id}"
+echo "------->>> TF_VAR_fingerprint= ${TF_VAR_fingerprint}"
+echo "------->>> TF_VAR_private_key= ${TF_VAR_private_key}"
+echo "------->>> TF_VAR_ssh_authorized_keys= ${TF_VAR_ssh_authorized_keys}"
 echo "------->>> TF_VAR_ssh_private_key= ${TF_VAR_ssh_private_key}"
+ENV="$(env)"
+echo "------->>> ENV= ${ENV}"
 
 export TF_VAR_tenancy_id='ocid1.tenancy.oc1..aaaaaaaaa3qmjxr43tjexx75r6gwk6vjw22ermohbw2vbxyhczksgjir7xdq'
 
@@ -57,11 +59,10 @@ export TF_VAR_ssh_authorized_keys='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAnb7/OPSHI
 export TF_VAR_ssh_private_key=$'-----BEGIN RSA PRIVATE KEY-----\nMIIEoQIBAAKCAQEAnb7/OPSHIDalG5nQyOJi+5xkKvtGfRr7YOeVvdPxljrbvdzB\ndJ5+kNXljFI5cy/DZxpfKy7vBoxhSo2Un87eZkcd/TsqSEb92Pxmf4n+W9RXBW4W\nWxsncv9xQqyUVsK6qIN5ZwmXoXDutz2xfmNFsFBQYwYaPax8wsPJWxVj/9icO4wI\n+oiWrdDepLPVWYz+8mubhCO/S51IVl1agNT6s1sZqDVKTLTRNeLsMlkHJoVyQhjg\nWH1PNYxHgJbQR+JxDnp/H8YX7cUrrqjaYU3vCwCGCkFK2Q3RXGcE5QcyOUqzPFyR\nEkNUjuAAE+kYlBMjXLm+b/b6tn0okIAsKlfHCQIBJQKCAQEAkPSzLWsUYspuNQni\nc2g/SBMrnR5Axf0eWQwovEY38dU4oKFX0vKCJDos4c8EXAJgiEG/PHBRREmlgsdK\nTalWvtmRLeNXSVX+BamqS688wxYmc7FE+cXs5jbWx6WBZHuWxF0jc3CZLJFKECra\ndCPfLGV6TSgzyfhypSKdWp4I3UIkrpCAskg7SwOKY7gdzrU+jFUIRSCwps2Scnvg\n3IYqie+fewqukclWi9WhFBFKhPUU3uMzYvZkyljTTn9258VUp5VfBLUm6LIsHiB6\nVSTVfV2twknI+6U7gg8dDbtNltJm8Kg339En8K8+cHsD/lBXJgTSog0PbINbl4Me\nq0H4FQKBgQDTdE3H7+hYdb8oKmXn938X5vs4hoytJkLI/QfSDxbnvflx3mo1RAB5\nrWLbWmjHnrW8vaZ+8MJUmkElb05tYHobTOlaM9keMwRfEpH8gycW/tKOqHRSAlrX\nk4IUmCdeVi8JaULpCl8g7gND/UVjDYBWMSqfIs3ynTXgwgiyVC3iuwKBgQC++jeW\n3zOFOUKQ0Y0YPVe5kNedwWv3jfNB3L+qVJV7QSgu8RJIAKSIaMxK9s5fYIjEc8Zt\nP8z2Va48Hb2szRGXv+cQV75tgUozMAKU+fc7A8B+Dl6PECTPnT3nRXXhVZEMoAg+\nDMvihFpDbw7HxKTIr1yyjcB3UF36uKUFo1MLCwKBgHgDvXF6U3B6LjlkLAAyhmeD\nGPaRjh0VtzPN4dgWZvI7ZBAyIJrFuxSgrbrEnFWfRI23v1zN1hRXjMI4QUT/Z+X7\nOFXKZnjshfDFWcarTYmXjEMhVsbDEPblBKPnp6Q+wMAm/HZtq50Rd3mdlhWf4Q5T\nQbREL7M2ognxljuzPKNHAoGAUpW3LHwx9GvJwhVtckQKQmgl41qPjaUq7Axujth3\n/fKpl8Ixaz6MVqnbzWPO3SLTW98JsrPOQQJ027nVeyg+9YNq1qJ73FOVtUUxjIfE\n2z/kiYmsWYpwyHtZChAy+ah2E0wfPW1RP1vUAXwiETJwxXxDwtWDqTeCle77QLVU\nV80CgYAXGHPu1mY5hc63CxXirt1rh16ylxzAhbe2o5N5dxHqzp+qhdfnifxKup4d\nT9YDlZiJqJmTkuuqcd3FFvFLmvY9B3Ykwm1vQAdDLZ1mDk0AE4GSJYeZxnK2OX5Q\nSqeQ7RwHm9XM7y+gTGpY9oErDCVuC+WGDde+syHIrN6FYJdRgw==\n-----END RSA PRIVATE KEY-----\n'
 
 echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_tenancy_id}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_user_id}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_fingerprint}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_private_key}"
-echo "------->>> TF_VAR_tenancy_id= ${TF_VAR_ssh_authorized_keys}"
+echo "------->>> TF_VAR_user_id= ${TF_VAR_user_id}"
+echo "------->>> TF_VAR_fingerprint= ${TF_VAR_fingerprint}"
+echo "------->>> TF_VAR_private_key= ${TF_VAR_private_key}"
+echo "------->>> TF_VAR_ssh_authorized_keys= ${TF_VAR_ssh_authorized_keys}"
 echo "------->>> TF_VAR_ssh_private_key= ${TF_VAR_ssh_private_key}"
-ENV="$(env)"
-echo "------->>> TF_VAR_ssh_private_key= ${ENV}"
+
 go test -v $HOME/go/src/terratest/test/tf-oci-instance_test.go -timeout 20m
